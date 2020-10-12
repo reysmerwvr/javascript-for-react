00e36e48769919601cfe0df7f1ddcefcffe7edde
Reysmer Valle <reysmerwvr@gmail.com>
2020-10-11 20:27:40 -0500

    Add destructuring, spread, rest and default params exercises

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,34 @@
 function time() {
-    this.seg = 0;
-    setInterval(() => {
-        this.seg++; // undefined + 1
-        console.log(this.seg); // NaN
-    }, 1000);
-};
+  this.seg = 0;
+  setInterval(() => {
+    this.seg++; // undefined + 1
+    console.log(this.seg); // NaN
+  }, 1000);
+}
 
 function time1() {
-    const self = this;
-    self.seg = 0;
-    setInterval(() => {
-        self.seg++; // 0 + 1
-        console.log(this.seg); // 1, 2, 3
-    }, 1000);
-};
+  const self = this;
+  self.seg = 0;
+  setInterval(() => {
+    self.seg++; // 0 + 1
+    console.log(this.seg); // 1, 2, 3
+  }, 1000);
+}
 
 function time2() {
-    this.seg = 0;
-    setInterval(() => {
-        self.seg++; // 0 + 1
-        console.log(this.seg); // 1, 2, 3
-    }, 1000);
-};
+  this.seg = 0;
+  setInterval(() => {
+    self.seg++; // 0 + 1
+    console.log(this.seg); // 1, 2, 3
+  }, 1000);
+}
 
-const $user = document.querySelector('#user');
-$user.addEventListener('click', () => {
-    console.log(this.id); // Undefined
+const $user = document.querySelector("#user");
+$user.addEventListener("click", () => {
+  console.log(this.id); // Undefined
 });
 
-const $button = document.querySelector('#button');
-$user.addEventListener('click', function() {
-    console.log(this.id); // #button
+const $button = document.querySelector("#button");
+$user.addEventListener("click", function () {
+  console.log(this.id); // #button
 });
